0c08cf9f76410ea1b2ef371249e155ede0ac2c6b
maxaradze23 <119763321+maxaradze23@users.noreply.github.com>
2023-01-23 11:37:31 -0800

    created app.js

---
+++ app.js
@@ -0,0 +1,41 @@
+// // 1
+let num = 100
+for (let i = 0; i < 101; i++) {
+    console.log(i);
+}
+
+
+// // 2
+// let i = 0
+// while (i < 50){
+//     console.log(i)
+//     i++
+// }
+
+
+// 3
+// let i = 0
+// do {
+//   console.log(i)
+//   i++
+// }
+// while (i < 150)
+
+
+// // 4
+// let arr = []
+// for (let i = 0; i < 10000; i++) {
+//     arr.push(i * i);
+// }
+// console.log(arr)
+
+
+// // // 5
+// let arr = ["Nadal", "Djokovic", "Murray", "Wawrinka", "Thiem"]
+// arr.pop()    // 
+// // arr.shift()   //
+// // arr.unshift("Medvedev")   // 
+// // arr.push("Alcaraz")   // 
+
+// console.log(arr)
+
